193db03ea6b0f5c04a167c4c67f76800d6e22f16
Alejandro Alvarez <a.alejandro.alvarez.a@gmail.com>
2025-04-23 13:36:49 -0400

    feat(plane): add model plane

--- src/components/voxel-entity/goal/goalModels/Airplane/AirplaneComplete.tsx
+++ src/components/voxel-entity/goal/goalModels/Airplane/AirplaneComplete.tsx
@@ -1,28 +1,27 @@
 "use client";
-
 export const AirplaneComplete = () => {
   return (
     <group>
       {/* Cuerpo */}
       <mesh position={[0, 0.5, 0]}>
-        <boxGeometry args={[2, 0.5, 0.5]} />
+        <boxGeometry args={[1.5, 0.5, 0.5]} />
         <meshStandardMaterial color="#FACC15" />
       </mesh>
 
       {/* Alas */}
-      <mesh position={[0, 0.5, -0.75]}>
-        <boxGeometry args={[1.5, 0.1, 0.5]} />
+      <mesh position={[0, 0.5, -0.5]}>
+        <boxGeometry args={[0.5, 0.1, 1.5]} />
         <meshStandardMaterial color="#FCD34D" />
       </mesh>
 
-      <mesh position={[0, 0.5, 0.75]}>
-        <boxGeometry args={[1.5, 0.1, 0.5]} />
+      <mesh position={[0, 0.5, 0.5]}>
+        <boxGeometry args={[0.5, 0.1, 1.5]} />
         <meshStandardMaterial color="#FCD34D" />
       </mesh>
 
       {/* Cola */}
-      <mesh position={[-0.9, 1, 0]}>
-        <boxGeometry args={[0.2, 0.7, 0.2]} />
+      <mesh position={[-0.85, 0.7, 0]}>
+        <boxGeometry args={[0.2, 0.5, 0.2]} />
         <meshStandardMaterial color="#D97706" />
       </mesh>
     </group>
