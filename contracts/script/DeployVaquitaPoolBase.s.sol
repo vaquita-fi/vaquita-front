8521cb31caf54d8885fe54dedc5bde558e1e7ccc
raptor0929 <flaura.dev@gmail.com>
2025-07-02 21:22:56 -0400

    feat(blockchain): update deployment scripts for base mainnet

--- contracts/script/DeployVaquitaPoolBase.s.sol
+++ contracts/script/DeployVaquitaPoolBase.s.sol
@@ -6,7 +6,7 @@ import {VaquitaPool} from "../src/VaquitaPool.sol";
 import {TransparentUpgradeableProxy} from "../lib/openzeppelin-contracts/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 import {ProxyAdmin} from "../lib/openzeppelin-contracts/contracts/proxy/transparent/ProxyAdmin.sol";
 
-contract VaquitaPoolProxyScript is Script {
+contract DeployVaquitaPoolBaseScript is Script {
     function run() public returns (address) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address owner = vm.addr(deployerPrivateKey);
@@ -17,15 +17,13 @@ contract VaquitaPoolProxyScript is Script {
         console.log("VaquitaPool implementation:", address(implementation));
 
         // Encode initializer data
-        address aavePool = 0x07eA79F68B2B3df564D0A34F8e19D9B1e339814b; // Base Sepolia Aave V3 Pool
-        address token = 0x036CbD53842c5426634e7929541eC2318f3dCF7e; // Base Sepolia USDC
-        address aToken = 0xf53B60F4006cab2b3C4688ce41fD5362427A2A66; // Base Sepolia aUSDC
+        address aavePool = 0xA238Dd80C259a72e81d7e4664a9801593F98d1c5; // Base Aave V3 Pool
+        address token = 0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913; // Base USDC
         uint256 lockPeriod = 1 days;
         bytes memory initData = abi.encodeWithSelector(
             implementation.initialize.selector,
             token,
             aavePool,
-            aToken,
             lockPeriod
         );
 
